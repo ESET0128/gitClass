70ea4ad8c575d3061663b7bd6a29391e827c7f5f
EST0128 <Saurabh.Kumar1@esyasoft.com>
2025-11-13 12:56:42 +0530

    abc

--- index.py
+++ index.py
@@ -1,17 +1,6 @@
 cerjfdeyvbud
 svhcdbjvsdvudys
-<<<<<<< HEAD
 i am saurabh kumar. IIT Indore
-=======
-
-
- djv dfvda
-vdsajsvah
-dsvjdshv sadiv
-dviadviadiv
-
-dvjkdaj vkadf v
->>>>>>> e6e33ccd92440b3c7575dfabac5be330434cc008
 
 
 
